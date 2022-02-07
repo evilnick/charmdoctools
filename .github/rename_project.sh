4f980d0ace6be99b107ccec4af342529dfa234c7
evilnick <evilnick@users.noreply.github.com>
2022-02-07 14:23:26 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="evilnick"
+original_name="charmdoctools"
+original_urlname="charmdoctools"
+original_description="Awesome charmdoctools created by evilnick"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv charmdoctools $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- charmdoctools/base.py
+++ charmdoctools/base.py
@@ -1,7 +1,7 @@
 """
-project_name base module.
+charmdoctools base module.
 
-This is the principal module of the project_name project.
+This is the principal module of the charmdoctools project.
 here you put your main classes and objects.
 
 Be creative! do whatever you want!
