07524f900964ca12dd1621b733c1c4aa258195ad
Nick Veitch <nick.veitch@canonical.com>
2022-02-22 17:30:33 +0000

    version

--- charmdoctools/__main__.py
+++ charmdoctools/__main__.py
@@ -2,8 +2,10 @@ import click  # pragma: no cover
 from . import BaseClass, base_function  # pragma: no cover
 import pkgutil
 
-__version__ = str(pkgutil.get_data(__name__, "VERSION").decode("utf-8")).strip()
-
+try:
+    __version__ = str(pkgutil.get_data(__name__, "VERSION"), 'utf-8').strip()
+except:
+    __version__ = "local dev"
 
 @click.command()
 def main() -> None:
