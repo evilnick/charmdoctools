4920904a85c8f9995c5955489397e93014b5e6c0
Nick Veitch <nick.veitch@canonical.com>
2022-02-24 14:43:47 +0000

    add command group

--- charmdoctools/__main__.py
+++ charmdoctools/__main__.py
@@ -5,18 +5,25 @@ from . import BaseClass, base_function  # pragma: no cover
 v = pkgutil.get_data(__name__, "VERSION")
 __version__ = str(v, "utf-8").strip()  # type: ignore
 
+@click.group()
+@click.pass_context
+def main(ctx) -> None:
+    """charmdoctools is a collection of utilities for working with documentation, 
+    managed and maintained by Canonical's technical authors."""
 
-@click.command()
-def main() -> None:
-    """Example script."""
+    ctx.ensure_object(dict)
+    #ctx.verbose = verbose
     click.echo("Hello World!")
     click.echo("Version is {}".format(__version__))
-    click.echo("Executing main function")
-    base = BaseClass()
-    click.echo(base.base_method())
-    click.echo(base_function())
-    click.echo("End of main function")
+    
+
+
+@main.command()
+@click.pass_context
+def version(ctx) -> None:
+    """Returns the current version."""
+    click.echo(click.style(f"{__version__}", bold=True))
 
 
 if __name__ == "__main__":  # pragma: no cover
-    main()
+    main(obj={})
